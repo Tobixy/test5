fbb59f6860fd2d5ecc3bf33900c57fb957f8a2f5
Tobixy <shivamrajput69bs@gmail.com>
2023-08-30 09:25:46 +0530

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,70 +1,80 @@
 from pyrogram import Client, filters
-from pyrogram.types import Message
-from auth import is_authorized, is_developer
-from character import CharacterManager
+from pyrogram.types import InlineQueryResultArticle, InputTextMessageContent, Message
+import random
+import time
 from database import DatabaseManager
+from character import CharacterManager
 
 app = Client("character_collector_bot")
 
-character_manager = CharacterManager()  # Initialize character manager
-db_manager = DatabaseManager()  # Initialize database manager
+db_manager = DatabaseManager()
+character_manager = CharacterManager()
 
-# Start command handler
+user_harems = {}
+spawn_time = 300  # Default spawn time in seconds
+
+# Command to start the bot
 @app.on_message(filters.command("start"))
 def start(bot, message: Message):
     bot.send_message(message.chat.id, "Welcome to Character Collector Bot!")
 
-# Handler for adding characters by ID
-@app.on_message(filters.command("add_character_by_id") & (is_authorized | is_developer))
-def add_character_by_id(bot, message: Message):
-    # Handle adding characters by ID here
-    pass
-
-# Handler for adding character images and ranks
-@app.on_message(filters.command("add_character_data") & (is_authorized | is_developer))
-def add_character_data(bot, message: Message):
-    # Handle adding character images and ranks here
-    pass
-
-# Handler for collecting characters by name
-@app.on_message(filters.command("collect_character"))
+# Collect a character by typing the name
+@app.on_message(filters.text)
 def collect_character(bot, message: Message):
     user_id = message.from_user.id
-    character_name = message.text.split(maxsplit=1)[1].strip()
+    character_name = message.text.strip()
 
-    collected = character_manager.collect_character(user_id, character_name)
+    collected = collect_character_by_name(user_id, character_name)
     if collected:
         bot.send_message(message.chat.id, f"You collected {character_name}!")
     else:
         bot.send_message(message.chat.id, f"{character_name} not found.")
 
-# Handler for gifting characters
-@app.on_message(filters.command("gift_character"))
-def gift_character(bot, message: Message):
-    # Handle gifting characters here
-    pass
+# Collect a character by name and add to the user's harem
+def collect_character_by_name(user_id, character_name):
+    character = character_manager.get_character_by_name(character_name)
+    if character:
+        if user_id not in user_harems:
+            user_harems[user_id] = []
+        user_harems[user_id].append(character)
+        return True
+    return False
 
-# Handler for trading characters
-@app.on_message(filters.command("trade_character"))
-def trade_character(bot, message: Message):
-    # Handle trading characters here
-    pass
+# Inline query to search for characters
+@app.on_inline_query()
+def inline_query(bot, query):
+    results = []
 
-# Handler for resetting player harem
-@app.on_message(filters.command("reset_harem") & (is_authorized | is_developer))
-def reset_harem(bot, message: Message):
-    user_id = message.reply_to_message.from_user.id
-    db_manager.reset_harem(user_id)
-    bot.send_message(message.chat.id, f"Harem of user {user_id} reset.")
+    if query.query:
+        characters = db_manager.search_characters(query.query)
+        for character in characters:
+            description = f"Anime: {character['anime']} | Rank: {character['rank']}"
+            results.append(
+                InlineQueryResultArticle(
+                    id=str(character['id']),
+                    title=character['name'],
+                    description=description,
+                    input_message_content=InputTextMessageContent(
+                        message_text=f"Character: {character['name']}\nAnime: {character['anime']}\nRank: {character['rank']}"
+                    )
+                )
+            )
 
-# Handler for displaying global leaderboard
-@app.on_message(filters.command("leaderboard"))
-def leaderboard(bot, message: Message):
-    # Handle displaying global leaderboard here
-    pass
+    bot.answer_inline_query(query.id, results=results, cache_time=1)
 
-# ... other handlers and logic ...
+# Automatic character spawning in all chats
+def spawn_characters_periodically():
+    while True:
+        for chat in app.get_dialogs():
+            if chat.chat.type == "group" or chat.chat.type == "supergroup":
+                spawn_random_character_img(app.send_photo, chat.chat.id)
+        time.sleep(spawn_time)
 
 if __name__ == "__main__":
+    # Start the thread to spawn characters periodically
+    import threading
+    character_thread = threading.Thread(target=spawn_characters_periodically)
+    character_thread.daemon = True
+    character_thread.start()
+    
     app.run()
-
