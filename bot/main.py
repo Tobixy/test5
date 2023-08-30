2f39239e144c3a3a017f07e963a150ea7896e7ff
Tobixy <shivamrajput69bs@gmail.com>
2023-08-30 08:46:06 +0530

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,1 +1,70 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from auth import is_authorized, is_developer
+from character import CharacterManager
+from database import DatabaseManager
+
+app = Client("character_collector_bot")
+
+character_manager = CharacterManager()  # Initialize character manager
+db_manager = DatabaseManager()  # Initialize database manager
+
+# Start command handler
+@app.on_message(filters.command("start"))
+def start(bot, message: Message):
+    bot.send_message(message.chat.id, "Welcome to Character Collector Bot!")
+
+# Handler for adding characters by ID
+@app.on_message(filters.command("add_character_by_id") & (is_authorized | is_developer))
+def add_character_by_id(bot, message: Message):
+    # Handle adding characters by ID here
+    pass
+
+# Handler for adding character images and ranks
+@app.on_message(filters.command("add_character_data") & (is_authorized | is_developer))
+def add_character_data(bot, message: Message):
+    # Handle adding character images and ranks here
+    pass
+
+# Handler for collecting characters by name
+@app.on_message(filters.command("collect_character"))
+def collect_character(bot, message: Message):
+    user_id = message.from_user.id
+    character_name = message.text.split(maxsplit=1)[1].strip()
+
+    collected = character_manager.collect_character(user_id, character_name)
+    if collected:
+        bot.send_message(message.chat.id, f"You collected {character_name}!")
+    else:
+        bot.send_message(message.chat.id, f"{character_name} not found.")
+
+# Handler for gifting characters
+@app.on_message(filters.command("gift_character"))
+def gift_character(bot, message: Message):
+    # Handle gifting characters here
+    pass
+
+# Handler for trading characters
+@app.on_message(filters.command("trade_character"))
+def trade_character(bot, message: Message):
+    # Handle trading characters here
+    pass
+
+# Handler for resetting player harem
+@app.on_message(filters.command("reset_harem") & (is_authorized | is_developer))
+def reset_harem(bot, message: Message):
+    user_id = message.reply_to_message.from_user.id
+    db_manager.reset_harem(user_id)
+    bot.send_message(message.chat.id, f"Harem of user {user_id} reset.")
+
+# Handler for displaying global leaderboard
+@app.on_message(filters.command("leaderboard"))
+def leaderboard(bot, message: Message):
+    # Handle displaying global leaderboard here
+    pass
+
+# ... other handlers and logic ...
+
+if __name__ == "__main__":
+    app.run()
 
